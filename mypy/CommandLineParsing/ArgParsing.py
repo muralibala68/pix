b0c462c25451d6be0cb4a2a274f17298d8f78677
murali <muralibala68@gmail.com>
2024-05-24 13:52:02 +0100

    command line argument parsing

---
+++ mypy/CommandLineParsing/ArgParsing.py
@@ -0,0 +1,11 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="Argument parsing example using argparse module")
+parser.add_argument("-f", "--first_name", default="first_name_not_given", help="First Name", type = str)
+parser.add_argument("-l", "--last_name", default="last_name_not_given", help="Last Name", type = str)
+parser.add_argument("-v", "--verbose", action="store_true", help="Verbose on")
+args = parser.parse_args()
+
+print("first_name:", args.first_name)
+print("last_name", args.last_name)
+print("verbose on?", args.verbose)
