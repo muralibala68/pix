0e0361e48cff1d1e4d9ca71f5d2e77853c3e33c1
murali <muralibala68@gmail.com>
2024-05-24 22:39:39 +0100

    generator expression

--- mypy/FluentPython/Factorial.py
+++ mypy/FluentPython/Factorial.py
@@ -9,8 +9,34 @@ print(factorial(3))
 
 # list comprehension
 print([factorial(n) for n in range(5)])
-# generator comprehension
-print((factorial(n) for n in range(5)))
-
-for x in (factorial(n) for n in range(5)):
+# generator expression
+ge = (factorial(n) for n in range(5))
+print(ge)
+for x in ge:
     print(x)
+
+print("one by one(1)...")
+ge = (factorial(n) for n in range(6))
+while True:
+    try:
+        print(next(ge))
+    except StopIteration:  # when you don't know the range
+        break
+print("done(1)")
+
+print("one by one(2)...")
+count = 6
+ge = (factorial(n) for n in range(count))
+for _ in range(count):
+    print(next(ge))
+print("done(2)")
+
+
+def simple_generator():
+    yield 1
+    yield 2
+    yield 3
+
+
+for vv in simple_generator():
+    print(vv)
