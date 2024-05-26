6913844b2ce73c3d00c4cbb29d4ce9bb9b2cf0a6
murali <muralibala68@gmail.com>
2024-05-26 13:36:16 +0100

    playing cards - dealing for 4 players

--- mypy/FluentPython/PlayingCards.py
+++ mypy/FluentPython/PlayingCards.py
@@ -1,49 +1,56 @@
 import random
 from collections import namedtuple
-from typing import List
+from typing import List, Tuple
 
 
 class FrenchDeck:
     Card = namedtuple('Card', ['rank', 'suit'])
-    Deck = List[Card]
+    Cards = List[Card]
 
     suits = ['spade', 'heart', 'diamond', 'club']
     ranks = [str(n) for n in range(2, 11)] + list("JQKA")
 
     def __init__(self):
-        self.deck: FrenchDeck.Deck = [self.Card(r, s) for r in self.ranks
-                                                      for s in self.suits]
+        self.cards: FrenchDeck.Cards = [self.Card(r, s) for r in self.ranks
+                                                        for s in self.suits]
 
     def __repr__(self):
-        return str(self.deck)
+        return str(self.cards)
 
     def __str__(self):
-        return str(self.deck)
+        return str(self.cards)
 
     def __len__(self):
-        return len(self.deck)
+        return len(self.cards)
 
     def __getitem__(self, index):
-        return self.deck[index]
+        return self.cards[index]
 
     def shuffle(self):
-        print(self.deck)
-        random.shuffle(self.deck)
-        print(self.deck)
+        random.shuffle(self.cards)
 
-    def deal(self) -> Card:
-        return random.choice(self.deck)
+    def deal(self) -> Tuple[Cards, Cards, Cards, Cards]:
+        return self.cards[0::4], self.cards[1::4], self.cards[2::4], self.cards[3::4]
 
 
 def main():
+    print(FrenchDeck)
     f_deck = FrenchDeck()
     print(f_deck)
     print(len(f_deck))
-    f_deck.shuffle()
-    print(f_deck.deal())
     print(f_deck[0])
     print(f_deck[1::2])
-    print(FrenchDeck)
+
+    print("-----------------")
+    print("before shuffling:", f_deck)
+    f_deck.shuffle()
+    print("after shuffling:", f_deck)
+    players: (FrenchDeck.Cards, FrenchDeck.Cards, FrenchDeck.Cards, FrenchDeck.Cards) = f_deck.deal()
+    print("Players/Deal", type(players), "of size", len(players))
+    for player in players:
+        print("Player", type(player), "of size", len(player))
+        print(player)
+    print("-----------------")
 
 
 if __name__ == "__main__":
